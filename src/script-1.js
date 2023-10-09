682ac1748a8dd7d1462b1002b8ad6b2233333093
maxstefan007 <jooliantoo@gmail.com>
2023-10-09 09:29:16 +0700

    first commit

---
+++ src/script-1.js
@@ -0,0 +1,4 @@
+const hello = () => console.log("hello");
+const world = () => console.log("world");
+export default hello;
+export { world };
---
+++ src/script-2.js
@@ -0,0 +1,9 @@
+function salam() {
+  return "selamat datang";
+}
+
+function jalan() {
+  return "selamat jalan";
+}
+
+export { salam, jalan };
